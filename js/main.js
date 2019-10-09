d3cfffaa8d891fc30c2d7134d75647671124ff70
William Chrapcynski <wchrapcynski@gmail.com>
2019-10-09 16:31:37 -0400

    Pseudocode

--- js/main.js
+++ js/main.js
@@ -92,13 +92,307 @@ var deck = [
         points: 2
     },
     {
-        rank: "one",
-        suit: "hearts",
-        cardImage: "images/1_of_hearts.png",
+        rank: "ace",
+        suit: "clubs",
+        cardImage: "images/ace_of_clubs.png",
         flipped: false,
-        points: 1
+        points: 14
     },
+    {
+        rank: "king",
+        suit: "clubs",
+        cardImage: "images/king_of_clubs.png",
+        flipped: false,
+        points: 13
+    },
+    {
+        rank: "queen",
+        suit: "clubs",
+        cardImage: "images/queen_of_clubs.png",
+        flipped: false,
+        points: 12
+    },
+    {
+        rank: "jack",
+        suit: "clubs",
+        cardImage: "images/jack_of_clubs.png",
+        flipped: false,
+        points: 11
+    },
+    {
+        rank: "ten",
+        suit: "clubs",
+        cardImage: "images/10_of_clubs.png",
+        flipped: false,
+        points: 10
+    },
+    {
+        rank: "nine",
+        suit: "clubs",
+        cardImage: "images/9_of_clubs.png",
+        flipped: false,
+        points: 9
+    },
+    {
+        rank: "eight",
+        suit: "clubs",
+        cardImage: "images/8_of_clubs.png",
+        flipped: false,
+        points: 8
+    },
+    {
+        rank: "seven",
+        suit: "clubs",
+        cardImage: "images/7_of_clubs.png",
+        flipped: false,
+        points: 7
+    },
+    {
+        rank: "six",
+        suit: "clubs",
+        cardImage: "images/6_of_clubs.png",
+        flipped: false,
+        points: 6
+    },
+    {
+        rank: "five",
+        suit: "clubs",
+        cardImage: "images/5_of_clubs.png",
+        flipped: false,
+        points: 5
+    },
+    {
+        rank: "four",
+        suit: "clubs",
+        cardImage: "images/4_of_clubs.png",
+        flipped: false,
+        points: 4
+    },
+    {
+        rank: "three",
+        suit: "clubs",
+        cardImage: "images/3_of_clubs.png",
+        flipped: false,
+        points: 3
+    },
+    {
+        rank: "two",
+        suit: "clubs",
+        cardImage: "images/2_of_clubs.png",
+        flipped: false,
+        points: 2
+    },
+    {
+        rank: "ace",
+        suit: "diamonds",
+        cardImage: "images/ace_of_diamonds.png",
+        flipped: false,
+        points: 14
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king_of_diamonds.png",
+        flipped: false,
+        points: 13
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen_of_diamonds.png",
+        flipped: false,
+        points: 12
+    },
+    {
+        rank: "jack",
+        suit: "diamonds",
+        cardImage: "images/jack_of_diamonds.png",
+        flipped: false,
+        points: 11
+    },
+    {
+        rank: "ten",
+        suit: "diamonds",
+        cardImage: "images/10_of_diamonds.png",
+        flipped: false,
+        points: 10
+    },
+    {
+        rank: "nine",
+        suit: "diamonds",
+        cardImage: "images/9_of_diamonds.png",
+        flipped: false,
+        points: 9
+    },
+    {
+        rank: "eight",
+        suit: "diamonds",
+        cardImage: "images/8_of_diamonds.png",
+        flipped: false,
+        points: 8
+    },
+    {
+        rank: "seven",
+        suit: "diamonds",
+        cardImage: "images/7_of_diamonds.png",
+        flipped: false,
+        points: 7
+    },
+    {
+        rank: "six",
+        suit: "diamonds",
+        cardImage: "images/6_of_diamonds.png",
+        flipped: false,
+        points: 6
+    },
+    {
+        rank: "five",
+        suit: "diamonds",
+        cardImage: "images/5_of_diamonds.png",
+        flipped: false,
+        points: 5
+    },
+    {
+        rank: "four",
+        suit: "diamonds",
+        cardImage: "images/4_of_diamonds.png",
+        flipped: false,
+        points: 4
+    },
+    {
+        rank: "three",
+        suit: "diamonds",
+        cardImage: "images/3_of_diamonds.png",
+        flipped: false,
+        points: 3
+    },
+    {
+        rank: "two",
+        suit: "diamonds",
+        cardImage: "images/2_of_diamonds.png",
+        flipped: false,
+        points: 2
+    },
+    {
+        rank: "ace",
+        suit: "spades",
+        cardImage: "images/ace_of_spades.png",
+        flipped: false,
+        points: 14
+    },
+    {
+        rank: "king",
+        suit: "spades",
+        cardImage: "images/king_of_spades.png",
+        flipped: false,
+        points: 13
+    },
+    {
+        rank: "queen",
+        suit: "spades",
+        cardImage: "images/queen_of_spades.png",
+        flipped: false,
+        points: 12
+    },
+    {
+        rank: "jack",
+        suit: "spades",
+        cardImage: "images/jack_of_spades.png",
+        flipped: false,
+        points: 11
+    },
+    {
+        rank: "ten",
+        suit: "spades",
+        cardImage: "images/10_of_spades.png",
+        flipped: false,
+        points: 10
+    },
+    {
+        rank: "nine",
+        suit: "spades",
+        cardImage: "images/9_of_spades.png",
+        flipped: false,
+        points: 9
+    },
+    {
+        rank: "eight",
+        suit: "spades",
+        cardImage: "images/8_of_spades.png",
+        flipped: false,
+        points: 8
+    },
+    {
+        rank: "seven",
+        suit: "spades",
+        cardImage: "images/7_of_spades.png",
+        flipped: false,
+        points: 7
+    },
+    {
+        rank: "six",
+        suit: "spades",
+        cardImage: "images/6_of_spades.png",
+        flipped: false,
+        points: 6
+    },
+    {
+        rank: "five",
+        suit: "spades",
+        cardImage: "images/5_of_spades.png",
+        flipped: false,
+        points: 5
+    },
+    {
+        rank: "four",
+        suit: "spades",
+        cardImage: "images/4_of_spades.png",
+        flipped: false,
+        points: 4
+    },
+    {
+        rank: "three",
+        suit: "spades",
+        cardImage: "images/3_of_spades.png",
+        flipped: false,
+        points: 3
+    },
+    {
+        rank: "two",
+        suit: "spades",
+        cardImage: "images/2_of_spades.png",
+        flipped: false,
+        points: 2
+    }
 ];
+var numberOfPlayers = 2;
+
+//reset the game
+rest();
+
+// Give instructions on how to play the game
+
+// Each play clicks on the top card of their deck to flip one over. This should also calculate which card is the highest and adds a point to the users score.
+
+// Players click next to show back of cards for the next round.
+
+// When all cards are used, final score is displayed and the winner is declared.
+
+// **These are functions and game logic**
+// Function to reset the game - Shuffles deck, etc.
+function reset() {
+    shuffle(deck);
+
+    // This code will be for setting up player deck arrays.
+    // for(let i = 1; i <= numberOfPlayers; i++) {
+    //      eval(player + i + " = val[i]");
+    // }
+}
+
+// Call this when a player clicks on a card to flip it over.
+function flipCard() {
+
+}
 
 // Fisher-Yates Shuffle
 function shuffle(array) {
