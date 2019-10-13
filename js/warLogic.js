83bde3716bd3cc89161804f62914987cc6fe2d3d
William Chrapcynski <wchrapcynski@gmail.com>
2019-10-13 16:53:51 -0400

    Main logic done and working

--- js/warLogic.js
+++ js/warLogic.js
@@ -78,7 +78,7 @@ class Game {
         console.log("Let's start the game! Use the game.flip() a player flip over a card(starting with Player 1).")
     }
 
-    // Flips over a card of the current player. If both players have flipped over a card, there will be a comparison. To make this game for 3+ players, this function would have to expanded upon.
+    // Flips over a card of the current player. If both players have flipped over a card, there will be a comparison. To make this game for 3+ players, this function would have to expanded upon. This is really the heart of the game.
     flip(x = this.currentPlayer, y = this.playerArray) {
         if(this.gameOver === true) {
             console.log("The game is over! Refresh the page to start a new game!")
@@ -87,23 +87,27 @@ class Game {
             if(x < this.numberOfPlayers) {
                 this.currentPlayer += 1;
             } else if (this.currentPlayer === this.numberOfPlayers) {
+                // Player one wins the round
                 if(y[1][0].score > y[2][0].score) {
                     console.log("Player 1 has the highest card!")
                     this.playerOneRoundWin(y);
                     this.displayCardCount();
                     this.currentPlayer = 1;
                     this.isGameOver();
+                // Plalyer two wins the round
                 } else if (y[2][0].score > y[1][0].score){
                     console.log("Player 2 has the highest card!")
                     this.playerTwoRoundWin(y)
                     this.displayCardCount();
                     this.currentPlayer = 1;
                     this.isGameOver();
+                // Runs through a War scenario
                 } else {
                     console.log("Let's get it on!");
                     console.log("It's time for War!");
                     console.log("Each player is forced to draw three cards face down  and then flip over the fourth");
                     this.enoughForWar();
+                    // War count is here in case a tie happens in War itself. This will let the process loop properly and moving around the correct amount of cards.
                     let warCount = 0;
                     let warIsOver = false;
                     while (this.gameOver === false && warIsOver === false ) {
@@ -111,30 +115,27 @@ class Game {
                         console.log("Player 1 flips over the " + y[1][4 + warCount].rank + " of " + y[1][4 + warCount].suit + "!");
                         console.log("Player 2 flips over the " + y[2][4 + warCount].rank + " of " + y[2][4 + warCount].suit + "!");
                         if(y[1][4 + warCount].score > y[2][4 + warCount].score) {
+                            // Player one wins the round
                             console.log("Player 1 has the highest card!")
                             for (let i = 0; i <= 4 + warCount; i ++) {
-                                y[1].push(y[2][0]);
-                                y[2].shift();
-                                y[1].push(y[1][0]);
-                                y[1].shift();
+                                this.playerOneRoundWin(y);
                             }
                             this.displayCardCount();
                             this.currentPlayer = 1;
                             warIsOver = true;
                             this.isGameOver();
                         } else if (y[2][4 + warCount].score > y[1][4 + warCount].score){
+                            // Runs through a War scenario
                             console.log("Player 2 has the highest card!")
                             for (let i = 0; i <= 4 + warCount; i ++) {
-                                y[2].push(y[1][0]);
-                                y[1].shift();
-                                y[2].push(y[2][0]);
-                                y[2].shift();
+                                this.playerTwoRoundWin(y)
                             }
                             this.displayCardCount();
                             this.currentPlayer = 1;
                             warIsOver = true;
                             this.isGameOver();
                         } else {
+                            // If a tie again, War loops
                             console.log("The War rages on!");
                             warCount += 4;
                             this.enoughForWar();
@@ -145,12 +146,14 @@ class Game {
         }
     }
 
+    // Displays how many cards each player has left
     displayCardCount(x = this.numberOfPlayers, y = this.playerArray) {
         for(let i = 1; i <= x; i ++) {
             console.log("Player " + i + " has " + y[i].length + " cards.")
         }
     }
 
+    // If a player does not have enough cards for War, the game ends
     enoughForWar(x = this.numberOfPlayers, y = this.playerArray) {
         for(let i = 1; i <= x; i++) {
             if(y[i].length < 4) {
@@ -160,6 +163,7 @@ class Game {
         }
     }
 
+    // Checks to see if a player has run out of cards
     isGameOver(x = this.numberOfPlayers, y = this.playerArray) {
         if(this.playerArray[1].length === 0) {
             console.log("Player 2 Wins!")
@@ -170,13 +174,14 @@ class Game {
         }
     }
 
+    // Moving of cards if player one wins
     playerOneRoundWin(y) {
         y[1].push(y[2][0]);
         y[2].shift();
         y[1].push(y[1][0]);
         y[1].shift();
     }
-
+    // Moving of cards if player two wins
     playerTwoRoundWin(y) {
         y[2].push(y[1][0]);
         y[1].shift();
